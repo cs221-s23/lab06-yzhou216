5c4dcafe2c601f116fe5f2961c0808405461efa0
Yiyu Zhou <yzhou155@vlab00.cs.usfca.edu>
2023-04-11 01:57:08 -0700

    Add scan_token

--- scan.c
+++ scan.c
@@ -73,28 +73,83 @@ char *scan_whitespace(char *p, char *end)
 	return p;
 }
 
-char *scan_integer(char *p, char *end, struct scan_token_st *tp)
+char *scan_intlit(struct scan_token_st *tp, char *p, char *end)
 {
-	int i = 0;
-	while (scan_is_digit(*p) && p < end) {
-		tp->name[i] = *p;
-		p += 1;
-		i += 1;
-	}
-	tp->name[i] = '\0';
 	tp->id = TK_INTLIT;
+
+	int i;
+	for (i = 0; p < end; i++) {
+		if (!scan_is_digit(*p))
+			break;
+		tp->name[i] = *p;
+		p++;
+	}
+	tp->name[i + 1] = '\0';
+
 	return p;
 }
 
-char *scan_token(char *p, char *end, struct scan_token_st *tp)
+char *scan_binlit(struct scan_token_st *tp, char *p, char *end)
+{
+	tp->id = TK_BINLIT;
+
+	p+=2; /* 0b */
+	int i;
+	for (i = 0; p < end; i++) {
+		if (*p != '0' && *p != '1')
+			break;
+		tp->name[i] = *p;
+		p++;
+	}
+	tp->name[i + 1] = '\0';
+
+	return p;
+}
+
+bool scan_is_alpha(char c)
+{
+	return c >= 'a' && c <= 'f';
+}
+
+char *scan_hexlit(struct scan_token_st *tp, char *p, char *end)
+{
+	tp->id = TK_HEXLIT;
+
+	p+=2; /* 0x */
+	int i;
+	for (i = 0; p < end; i++) {
+		if (!scan_is_digit(*p) && !scan_is_alpha(*p))
+			break;
+		tp->name[i] = *p;
+		p++;
+	}
+	tp->name[i + 1] = '\0';
+
+	return p;
+}
+
+
+
+char *scan_token(char *p, char *end, struct scan_token_st *tp )
 {
 	if (p == end) {
 		p = scan_read_token(tp, p, 0, TK_EOT);
 	} else if (scan_is_whitespace(*p)) {
+		/* skip over the whitespace */
 		p = scan_whitespace(p, end);
+		/* recurse to get the next token */
 		p = scan_token(p, end, tp);
+	} else if (*p == '0') {
+		if (*(p + 1) == 'x') {
+			p = scan_hexlit(tp, p, end);
+		} else if (*(p + 1) == 'b') {
+			p = scan_binlit(tp, p, end);
+		} else {
+			printf("unexpected token: %c\n", *(p + 1));
+			exit(-1);
+		}
 	} else if (scan_is_digit(*p)) {
-		p = scan_integer(p, end, tp);
+		p = scan_intlit(tp, p, end);
 	} else if (*p == '+') {
 		p = scan_read_token(tp, p, 1, TK_PLUS);
 	} else if (*p == '-') {
@@ -108,9 +163,9 @@ char *scan_token(char *p, char *end, struct scan_token_st *tp)
 	} else if (*p == ')') {
 		p = scan_read_token(tp, p, 1, TK_RPAREN);
 	} else {
-		scan_error("Invalid character");
+		printf("unexpected token: %c\n", *p);
+		exit(-1);
 	}
-
 	return p;
 }
 
