e6be433c1fbcdbb145733d09c2cc1fa8d6aad93e
Yiyu Zhou <yzhou155@vlab00.cs.usfca.edu>
2023-04-11 01:05:33 -0700

    Clean up

--- base.c
+++ base.c
@@ -1,6 +1,7 @@
 #include "lab06.h"
 
-int string_to_int(char *string, int base) {
-    //TODO: implement this
-    return 0;
+int string_to_int(char *string, int base)
+{
+	/* TODO: implement this */
+	return 0;
 }
--- lab06.h
+++ lab06.h
@@ -23,25 +23,25 @@
 
     Ignore
 
-    whitespace  =  (' ' | '\t') (' ' | '\t')* 
+    whitespace  =  (' ' | '\t') (' ' | '\t')*
 */
 
 #define SCAN_TOKEN_LEN 32
 #define SCAN_INPUT_LEN 2048
 
 enum scan_token_enum {
-    TK_INTLIT, // -123, 415
-    TK_BINLIT, // 0b10
-    TK_HEXLIT, // 0xFF
-    TK_LPAREN, // (
-    TK_RPAREN, // )
-    TK_PLUS,   // +
-    TK_MINUS,  // -
-    TK_MULT,   // *
-    TK_DIV,    // /
-    TK_EOT,    // end of text
-    TK_ANY,    // wildcard for parsing
-    NUM_TOKENS,
+	TK_INTLIT, /* -123, 415 */
+	TK_BINLIT, /* 0b10 */
+	TK_HEXLIT, /* 0xFF */
+	TK_LPAREN, /* ( */
+	TK_RPAREN, /* ) */
+	TK_PLUS,   /* + */
+	TK_MINUS,  /* - */
+	TK_MULT,   /* * */
+	TK_DIV,    /* / */
+	TK_EOT,    /* end of text */
+	TK_ANY,    /* wildcard for parsing */
+	NUM_TOKENS,
 };
 
 #define SCAN_TOKEN_STRINGS {"TK_INTLIT", "TK_BINLIT", "TK_HEXLIT", \
@@ -50,15 +50,15 @@ enum scan_token_enum {
                             "TK_EOT", "TK_ANY"};
 
 struct scan_token_st {
-    enum scan_token_enum id;
-    char name[SCAN_TOKEN_LEN];
-    struct scan_token_st *next;
+	enum scan_token_enum id;
+	char name[SCAN_TOKEN_LEN];
+	struct scan_token_st *next;
 };
 
 struct scan_table_st {
-    struct scan_token_st *head;
-    int len;
-    int next;
+	struct scan_token_st *head;
+	int len;
+	int next;
 };
 
 void scan_table_init(struct scan_table_st *st);
@@ -81,25 +81,27 @@ enum parse_expr_enum {EX_INTVAL, EX_OPER1, EX_OPER2};
 enum parse_oper_enum {OP_PLUS, OP_MINUS, OP_MULT, OP_DIV};
 
 struct parse_node_st {
-    enum parse_expr_enum type;
-    union {
-        struct {
-            int value;
-        } intval;
-        struct {
-            enum parse_oper_enum oper;
-            struct parse_node_st *operand;
-        } oper1;
-        struct {
-            enum parse_oper_enum oper;
-            struct parse_node_st *left;
-            struct parse_node_st *right;
-        } oper2;
-    };                
+	enum parse_expr_enum type;
+	union {
+		struct {
+			int value;
+		} intval;
+
+		struct {
+			enum parse_oper_enum oper;
+			struct parse_node_st *operand;
+		} oper1;
+
+		struct {
+			enum parse_oper_enum oper;
+			struct parse_node_st *left;
+			struct parse_node_st *right;
+		} oper2;
+	};
 };
 
 struct parse_table_st {
-    struct parse_node_st *root;
+	struct parse_node_st *root;
 };
 
 void parse_table_init(struct parse_table_st *pt);
@@ -108,7 +110,7 @@ void parse_tree_print(struct parse_node_st *np);
 struct parse_node_st *parse_program(struct scan_table_st *st);
 
 
-/* 
+/*
  * These prototypes are needed for the mutual recursion between
  * parse_expression() and parse_operand()
  */
@@ -116,7 +118,7 @@ struct parse_node_st *parse_program(struct scan_table_st *st);
 struct parse_node_st *parse_expression(struct scan_table_st *st);
 struct parse_node_st *parse_operand(struct scan_table_st *st);
 
-// Prototype for your implementation of string_to_int
+/* Prototype for your implementation of string_to_int */
 int string_to_int(char *string, int base);
 
-#endif // _LAB06_H
+#endif /* _LAB06_H */
